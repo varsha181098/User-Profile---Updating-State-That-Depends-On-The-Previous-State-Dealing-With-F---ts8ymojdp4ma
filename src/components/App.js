9af7e7ba09365e90c09ab4953e10db6f190c89d7
varsha181098 <118925989+varsha181098@users.noreply.github.com>
2023-01-02 16:47:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
-
+const UserContext = React.createContext({name:"Newton", age:3});
 const App = () => {
 
   return (
